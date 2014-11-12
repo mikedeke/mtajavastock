889c553872a6a2a8afa6a0454221be320f29217c
mike.deke <mike.dekel@gmail.com>
2014-11-12 14:39:02 +0200

    excercise 2

--- stoc-java-miedeke/src/mta/javacours/stock/mikedeke/Stoc_java_miedekeServlet.java
+++ stoc-java-miedeke/src/mta/javacours/stock/mikedeke/Stoc_java_miedekeServlet.java
@@ -1,5 +1,6 @@
 package mta.javacours.stock.mikedeke;
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
@@ -7,5 +8,13 @@ public class Stoc_java_miedekeServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
+		int num1;
+		int num2;
+		int num3;
+		num1 = 3;
+		num2 = 4;
+		num3 = 7;
+		int result = num3*(num1+num2); 
+		System.out.println("num3(num1+num2)= +reusult");
 	}
 }
